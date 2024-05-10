9ec3df674b102f3f7877f22487e710e8496815ee
StanislavStadnyk <stanislav.stadnyk@gmail.com>
2024-05-10 12:17:59 +1000

    Updates

---
+++ precache-manifest.34bf387a25ee8f84408bc178938fac46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "090d75bc14400281a078c11d9fecee79",
+    "url": "/pet-shop-react/index.html"
+  },
+  {
+    "revision": "be731027158cea5d0fe2",
+    "url": "/pet-shop-react/static/css/main.980bc7f1.chunk.css"
+  },
+  {
+    "revision": "122e62ee6def9cbf6bfe",
+    "url": "/pet-shop-react/static/js/2.10a0279f.chunk.js"
+  },
+  {
+    "revision": "58df58a6da595dc2a1df77cd373b1123",
+    "url": "/pet-shop-react/static/js/2.10a0279f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be731027158cea5d0fe2",
+    "url": "/pet-shop-react/static/js/main.a8dd2763.chunk.js"
+  },
+  {
+    "revision": "c038b54f14d64a9ecca7",
+    "url": "/pet-shop-react/static/js/runtime-main.51a01efd.js"
+  }
+]);
